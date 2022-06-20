c754fca69557fca56cc77f5eb346b13025b1c084
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-20 17:18:08 +0000

    async return values

---
+++ asyncBreeds.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log(`In readFile's Callback: it has the data.`);
+    //ISSUE: Returning from inner callback function, not breedDetailsFromFILE
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+}
+
+// we try to get the return value
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed)
+}
+
+breedDetailsFromFile('Bombay', printOutCatBreed);
